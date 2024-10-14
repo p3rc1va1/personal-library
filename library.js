0f9e605e583d9a5ab277a8bc5e76f644d2cb3812
p3rc1va1 <35celik.baha@gmail.com>
2024-10-14 21:07:59 +0300

    Some more dialog logic added

--- library.js
+++ library.js
@@ -19,3 +19,21 @@ const dialogContainer = document.getElementById("dialogContainer")
 addButton.addEventListener("click",()=>{
     dialogContainer.showModal()
 })
+
+const submitDialogBtn = document.getElementById("submitDialogBtn");
+const bookShelf = document.getElementById("app")
+const form = document.getElementById("formD")
+const author = document.getElementById("author")
+const name = document.getElementById("name")
+const read = document.getElementById("read")
+submitDialogBtn.addEventListener("click", ()=>{
+   myLibrary.push(new Book(author.value,name.value,read.value))
+   form.reset()
+   let abook = document.createElement("div")
+   abook.classList.add("abook")
+   abook.style.width = "10%"
+   abook.style.height = "20%"
+   abook.style.border = "solid"
+   bookShelf.appendChild(abook)
+   console.log(myLibrary)
+})
