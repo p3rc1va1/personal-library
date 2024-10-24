65a4d949ee552c77f184e3e490f7ef98ecab5526
p3rc1va1 <35celik.baha@gmail.com>
2024-10-24 20:02:41 +0300

    visually perfect but cant figure out the data retreival from the form just yet

--- library.js
+++ library.js
@@ -6,34 +6,44 @@ function Book(name,author,read) {
   this.read = read;
 }
 
-// function addBookToLibrary() {
-//   // do stuff here
-// }
-
 
 console.log(new Book("Norwegian wood", "Murakami", true));
 
 //ui interactions
 const addButton = document.getElementById("addabook");
-const dialogContainer = document.getElementById("dialogContainer")
+const dialogContainer = document.getElementById("dialogContainer");
 addButton.addEventListener("click",()=>{
-    dialogContainer.showModal()
-})
+    dialogContainer.showModal();
+});
 
-const submitDialogBtn = document.getElementById("submitDialogBtn");
-const bookShelf = document.getElementById("app")
-const form = document.getElementById("formD")
-const author = document.getElementById("author")
-const name = document.getElementById("name")
-const read = document.getElementById("read")
-submitDialogBtn.addEventListener("click", ()=>{
-   myLibrary.push(new Book(author.value,name.value,read.value))
-   form.reset()
-   let abook = document.createElement("div")
-   abook.classList.add("abook")
-   abook.style.width = "10%"
-   abook.style.height = "20%"
-   abook.style.border = "solid"
-   bookShelf.appendChild(abook)
-   console.log(myLibrary)
-})
+const bookShelf = document.getElementById("app");
+let form = document.getElementById("formD");
+const cancel = document.getElementById("cancel");
+
+cancel.addEventListener("click",()=>{
+  dialogContainer.close();
+  form.reset();
+});
+
+form.addEventListener("submit", ()=>{
+  console.log(form.notread.value)
+  myLibrary.push(new Book(form.author.value,form.name.value,form.read.value));
+  let labelAuthor = document.createElement("p");
+  let labelName = document.createElement("p")
+  labelAuthor.textContent = form.author.value
+  labelName.textContent = '"'+ form.name.value + '"'
+  form.reset();
+  let abook = document.createElement("div");
+  abook.classList.add("abook");
+  abook.style.border = "solid";
+  if(form.read.value == true){
+    abook.style.borderColor = "#380000"
+  }else{
+    abook.style.borderColor = "#002211"
+  }
+
+  abook.appendChild(labelAuthor);
+  abook.appendChild(labelName);
+  bookShelf.appendChild(abook);
+  console.log(myLibrary);
+});
