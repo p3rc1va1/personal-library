01b5fa5ebbe4283a52f5cd143ae2e693ac0a352f
p3rc1va1 <35celik.baha@gmail.com>
2024-10-25 20:33:56 +0300

    only the logic for read and remove button left, also need to add a bookID column and iterate the myLibrary list with that ID column

--- library.js
+++ library.js
@@ -19,31 +19,63 @@ addButton.addEventListener("click",()=>{
 const bookShelf = document.getElementById("app");
 let form = document.getElementById("formD");
 const cancel = document.getElementById("cancel");
+let readIconDom = document.querySelectorAll("fa-solid", "fa-book-open-reader" ,"fa-lg")
 
 cancel.addEventListener("click",()=>{
   dialogContainer.close();
   form.reset();
 });
 
-form.addEventListener("submit", ()=>{
-  console.log(form.notread.value)
-  myLibrary.push(new Book(form.author.value,form.name.value,form.read.value));
-  let labelAuthor = document.createElement("p");
-  let labelName = document.createElement("p")
-  labelAuthor.textContent = form.author.value
-  labelName.textContent = '"'+ form.name.value + '"'
-  form.reset();
+form.addEventListener("submit", (e)=>{
+
+  //parsing forms returned values
+  e.preventDefault();
+  var formData = new FormData(form);
+  var formObject = Object.fromEntries(formData);
+  
+  //creating the book object and pushing it to the library list
+  myLibrary.push(new Book(formObject.author,formObject.name,formObject.read));
+
+  //creating the dom items that will be in the book div
   let abook = document.createElement("div");
+  let labelAuthor = document.createElement("p");
+  let labelName = document.createElement("p");
+
+  let removeIcon = document.createElement("i");
+
+  removeIcon.classList.add("fa-solid", "fa-trash", "fa-lg");
+
+  labelAuthor.textContent = formObject.author;
+  labelName.textContent = '"'+ formObject.name + '"';
   abook.classList.add("abook");
   abook.style.border = "solid";
-  if(form.read.value == true){
-    abook.style.borderColor = "#380000"
-  }else{
-    abook.style.borderColor = "#002211"
-  }
 
+
+
+  //placing the form elements into its place
   abook.appendChild(labelAuthor);
   abook.appendChild(labelName);
+
+  abook.appendChild(removeIcon)
+
+  if(formObject.read == "true"){
+    abook.style.borderColor = "#06402B"
+    
+  }else{
+    abook.style.borderColor = "#8B0000"
+    let readIcon = document.createElement("i");
+    readIcon.classList.add("fa-solid", "fa-book-open-reader" ,"fa-lg");
+    abook.appendChild(readIcon)
+  }
+
   bookShelf.appendChild(abook);
   console.log(myLibrary);
+
+  //return the dialog and the form into its original state
+  dialogContainer.close()
+  form.reset();
 });
+
+readIconDom.addEventListener("click", ()=>{
+  alert("clicked")
+})
